537edf7d5ef7b385e1987a27d777cddb059e7f90
deepikatadi <146415536+deepikatadi@users.noreply.github.com>
2025-03-22 13:40:24 +0530

    Add files via upload

---
+++ program_14.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract demo {
+
+    function f1() public pure returns (uint) {
+        require(true == true, "true is not true"); 
+        return 1;
+    }
+    function f2() public pure returns (uint) {
+        require(true == true, "true is not true"); 
+        return 2;
+    }
+    function f3() public pure returns (uint) {
+        require(true == true, "true is not true"); 
+        return 3;
+    }
+    function f4() public pure returns (uint) {
+        require(true == true, "true is not true"); 
+        return 4;
+    }
+}
